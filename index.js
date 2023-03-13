fe7a1f57a039cbffd5218c77148eb818ae69825c
Nicholas Cassiani <ndcassiani@gmail.com>
2023-03-13 13:45:01 -0500

    now no-side-effects is the only way to run it

--- index.js
+++ index.js
@@ -1,61 +1,31 @@
-const _ = require('lodash/fp');
-const parseNumbers = (x) => parseInt(x) || x;
-const removeSpaces = _.replace(/\s/g, '');
-const tokenize = _.pipe(removeSpaces, _.split(''), _.map(parseNumbers));
+const calculate = require('./no-side-effects');
+const readline = require('readline');
 
-const precedence = (x) => ({
-  '-': 1,
-  '+': 2,
-  '/': 3,
-  '*': 4
-})[x];
-
-const operators = ['-', '+', '/', '*'];
-const evalops = {
-  '-': (a, b) => a - b,
-  '+': (a, b) => a + b,
-  '/': (a, b) => a / b,
-  '*': (a, b) => a * b
+var rl = readline.createInterface(process.stdin, process.stdout)
+const getInput = () => {
+  const exit = ['quit', 'q', 'exit', 'done', 'stop', ':q'];
+  rl.question('> ', (eq) => {
+    if (exit.includes(eq)) {
+      process.exit();
+    } else {
+      try {
+        let ans = calculate(eq); 
+        let output = (typeof ans === 'number') ? ans : 'error';
+        console.log(`  :: ${output}`);
+        getInput();
+      } catch (error) {
+        console.log('  :: error');
+        console.log(error);
+        getInput();
+      }
+    }
+  });
 }
 
-const initShunt = (x) => ({ input: x, output: [], op: [] })
-const input2output = ({ input, output, op }) =>  ({
-  output: output.concat(_.head(input)),
-  input: _.tail(input),
-  op
-});
-const input2op = ({input, output, op}) => ({
-  output,
-  input: _.tail(input),
-  op: _.concat(op, _.head(input)),
-});
-const op2output = ({input, output, op}) => ({
-  output: _.concat(output, _.last(op)),
-  input,
-  op: _.initial(op)
-});
+//console.log(calculate('5+7*2'));
 
-const handleOps = (yard) =>
-  (precedence(_.first(yard.input)) < precedence(_.last(yard.op)))
-    ? op2output(yard)
-    : input2op(yard);
-
-const handleInput = (yard) => 
-  _.isNumber(_.first(yard.input)) ? input2output(yard) : handleOps(yard);
-
-const shunt = (yard) => 
-  _.isEmpty(yard.input) ? op2output(yard) : handleInput(yard); 
-
-const parse = (yard) => 
-  (_.isEmpty(yard.input) && _.isEmpty(yard.op)) 
-    ? yard.output
-  : _.pipe(shunt, parse)(yard);
-
-const rpn = _.pipe(tokenize, initShunt, parse);
-
-const rpnEvalStep = ({stack, in}) => 
-  _.includes(_.first(in), operators)
-    ?  { stack: [evalops[_.first(in)]()] }
-
-console.log(rpn('1 + 34 * 67'));
-
+let run = true;
+if (run) {
+  run = false;
+  getInput();
+}
